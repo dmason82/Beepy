a8f353d5ae27afd66485560b3f4255824a154ace
Doug Mason <d.mason82@gmail.com>
2012-02-20 20:47:28 -0800

    Initial commit with layout

---
+++ src/aad/uw/CP210/hw004/Homework004Activity.java
@@ -0,0 +1,26 @@
+package aad.uw.CP210.hw004;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class Homework004Activity extends Activity implements OnClickListener {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()){
+		
+		}
+		
+	}
+
+
+}
