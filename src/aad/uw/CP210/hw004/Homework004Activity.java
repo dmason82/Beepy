54f432209b2a18a77b43927b331f0af7089c94a3
Doug Mason <d.mason82@gmail.com>
2012-02-21 22:25:02 -0800

    Service setup

--- src/aad/uw/CP210/hw004/Homework004Activity.java
+++ src/aad/uw/CP210/hw004/Homework004Activity.java
@@ -1,26 +1,58 @@
 package aad.uw.CP210.hw004;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.content.Intent;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Homework004Activity extends Activity implements OnClickListener {
+	Intent i;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Button b =(Button) findViewById(R.id.button1);
+        b.setOnClickListener(this);
+       i = new Intent().setClass(getApplicationContext(), BeepService.class);
     }
 
 	@Override
 	public void onClick(View v) {
+		Button b =(Button) findViewById(R.id.button1);
 		switch(v.getId()){
-		
+		case(R.id.button1):
+			if(serviceRunning()){
+				b.setText("Start");
+				Toast t = Toast.makeText(getBaseContext(), "stopping", Toast.LENGTH_SHORT);
+				t.show();
+				stopService(i);
+			}
+			else
+			{
+				b.setText("Stop");
+				Toast t = Toast.makeText(getBaseContext(),"starting",Toast.LENGTH_SHORT);
+				t.show();
+				startService(i);
+			}
+			break;
 		}
 		
 	}
-
+	private boolean serviceRunning(){
+		ActivityManager manager = (ActivityManager)getSystemService(ACTIVITY_SERVICE);
+		for(RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)){
+			if("aad.uw.CP210.hw004.BeepService".equals(service.service.getClassName())){
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
